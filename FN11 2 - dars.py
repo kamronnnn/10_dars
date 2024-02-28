2afe4f49b5531b69a0f763c9beb10a3dac94573e
kamronnnn <kamronqodirov09@gmail.com>
2024-02-28 20:46:34 +0500

    10_dars qilindi

---
+++ FN11 2 - dars.py
@@ -0,0 +1,56 @@
+# file = open('bobur.txt')
+# content = file.read()
+import filecmp
+
+# print(content)
+# file.close()
+
+
+# file = open('navoiy.txt', encoding='utf-8')
+# print(file)
+# navoiy1 = file.read()
+#
+# print(navoiy1)
+# file.close()
+
+#=================================================================
+
+# file = open('name.txt', mode='w')
+# print(file)
+# file.write('Hello my name is Kamron')
+#
+# file.close()
+
+#=================================================================
+
+# file = open('navoiy.txt', mode='r', encoding='utf-8')
+# content = file.read()
+#
+# navoiy_copy = open('navoiy_copy.txt', mode='w', encoding='utf-8')
+# navoiy_copy.write(content)
+#
+# file.close()
+# navoiy_copy.close()
+
+#=================================================================
+
+# import csv
+# with open('user_info.csv', mode='r', encoding='utf-8') as csv_file:
+#     reader = csv.reader(csv_file)
+#     for i in reader:
+#         for j in i:
+#             print(j, end=' ')
+#         print()
+
+#=================================================================
+
+
+
+
+
+
+
+
+
+
+
